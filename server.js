6c43c019884e0a1c0d2bd217835000773f868c11
rachmad <rachmad.eepis@gmail.com>
2018-10-11 23:36:20 +0700

    edit package.json

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/ekita-dashboard'));
 
 app.get('/*all', function(req,res) {
     
-res.sendFile(path.join(__dirname + '.dist/ekita-dashboard/index.html'));
+res.sendFile(path.join(__dirname + '/dist/ekita-dashboard/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
