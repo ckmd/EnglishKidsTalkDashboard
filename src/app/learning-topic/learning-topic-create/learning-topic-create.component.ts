2222e73b4062ff74932d8dd9317aa5e3c51dd2bb
rachmad <rachmad.eepis@gmail.com>
2018-10-11 20:34:30 +0700

    able to upload image on learning topic

--- src/app/learning-topic/learning-topic-create/learning-topic-create.component.ts
+++ src/app/learning-topic/learning-topic-create/learning-topic-create.component.ts
@@ -2,30 +2,35 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { Location } from '@angular/common';
 import { LearningtopicService } from '../../service/learningtopic.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-learning-topic-create',
   templateUrl: './learning-topic-create.component.html',
   styleUrls: ['./learning-topic-create.component.css']
 })
+
 export class LearningTopicCreateComponent implements OnInit {
 
   constructor(
   	private formBuilder: FormBuilder,
   	private learningTopicService: LearningtopicService, 
-  	private location : Location) { }
-  addForm: FormGroup;
+  	private location : Location,
+    private http : HttpClient) { }
   
+  addForm: FormGroup;
+  selectedFile: File = null;
+
   ngOnInit() {
   	this.addForm = this.formBuilder.group({
 	    id: [],
 		question_difficulty_id: ['', Validators.required],
 		question_category_id: ['', Validators.required],
 		learning_topic_name: ['', Validators.required],
-		learning_topic_image: ['', Validators.required],
     });
   }
   onSubmit() {
+    this.onUpload();
     this.learningTopicService.createLearningTopic(this.addForm.value)
       .subscribe( data => {
       	this.goBack();
@@ -33,5 +38,16 @@ export class LearningTopicCreateComponent implements OnInit {
   }
   goBack(): void {
     this.location.back();
+  }
+  onFileChanged(event) {
+    this.selectedFile = event.target.files[0];
+    console.log(event);
+  }
+  onUpload(){
+    const uploadData = new FormData();
+    uploadData.append('learning_topic_image', this.selectedFile, this.selectedFile.name);
+    this.http.post('http://ekita-api.herokuapp.com/api/learning-topics', uploadData)
+    .subscribe( res => {console.log(res);}
+    );
   }
 }
