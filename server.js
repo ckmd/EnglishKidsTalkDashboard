3d7406dbaa0bfcb19ecbdaa54b4877136941e0ea
miftahunajat <miftahunajat@gmail.com>
2018-10-11 23:41:44 +0700

    modify server

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/ekidashboard'));
+app.use(express.static('./dist/ekita-dash'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/ekidashboard/index.html'));
+res.sendFile(path.join(__dirname,'/dist/ekita-dash/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
