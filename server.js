cfa87379b9d0dcb15e96d9c1adf9cee3873bdab4
rachmad <rachmad.eepis@gmail.com>
2018-10-11 23:03:24 +0700

    edit package.json

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/ekita-dashboard'));
 
-app.get('/*', function(req,res) {
+app.get('/*all', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/ekita-dashboard/index.html'));
+res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
