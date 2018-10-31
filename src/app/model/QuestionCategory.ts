e84c81258951b979de31a5fcd7d27fd8c65041d1
ckmd <rachmad.eepis@gmail.com>
2018-10-31 21:07:55 +0700

    adding dropdown list at create question category

--- src/app/model/QuestionCategory.ts
+++ src/app/model/QuestionCategory.ts
@@ -1,7 +1,9 @@
 export interface QuestionCategory
 {
     id: number;
+    question_difficulty_id: number;
     question_category_name: string;
+    star_needed: number;
     updatedAt: string;
     createdAt: string;
 }
--- src/app/question-category/question-category-create/question-category-create.component.ts
+++ src/app/question-category/question-category-create/question-category-create.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { QuestioncategoryService } from '../../service/questioncategory.service';
 import { Location } from '@angular/common';
+import { QuestionDifficultyService } from 'src/app/service/questiondifficulty.service';
 
 @Component({
   selector: 'app-question-category-create',
@@ -11,15 +12,22 @@ import { Location } from '@angular/common';
 export class QuestionCategoryCreateComponent implements OnInit {
 
   constructor(
-  	private formBuilder: FormBuilder,
+    private formBuilder: FormBuilder,
+    private qd : QuestionDifficultyService,
   	private questionCategoryService: QuestioncategoryService, 
   	private location : Location) { }
   addForm: FormGroup;
+  public questionDifficulties = [];
   
   ngOnInit() {
+    this.qd.getQuestionDifficulties()
+    .subscribe(questionDifficulties => this.questionDifficulties = questionDifficulties);
+
   	this.addForm = this.formBuilder.group({
-	    id: [],
-		question_category_name: ['', Validators.required],
+      id: [],
+      question_difficulty_id: ['', Validators.required],
+      question_category_name: ['', Validators.required],
+      star_needed: ['', Validators.required],
 	  });
   }
   onSubmit() {
